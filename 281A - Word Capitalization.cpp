ab495e961c49984a999bb9745e48309080c41b76
Bradley Allen <allenbradley91@hotmail.com>
2022-06-17 11:49:55 -0500

    Create 281A - Word Capitalization.cpp

---
+++ 281A - Word Capitalization.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+}//end of solve
+
+
+
+int main(){
+	
+	string word;
+
+	cin >> word;
+
+	word[0] = toupper(word[0]);
+
+	cout << word << endl;;
+	
+	}//end of main
+
+			
+
